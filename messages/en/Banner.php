26a8359f427da7b2c6c3932173aac5361bc84b0f
Andrew <andrew.panix@gmail.com>
2020-11-13 23:27:35 +0200

    init

---
+++ messages/en/Banner.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'IMAGE' => 'Image',
+    'CONTENT' => 'Content',
+    'CREATE_BANNER' => 'Creating a banner',
+    'UPDATE_BANNER' => 'Editing a banner',
+    'CREATED_AT' => 'Creation date',
+    'UPDATED_AT' => 'Date edited',
+    'ULR' => 'URL Link',
+    'URL_NAME' => 'Link name'
+];
+
